ca863021abe51aff968f111da5140864f0f6c63f
Luke Hands <lukehands98@gmail.com>
2022-07-09 17:58:27 +0100

    Started on the createman function

--- Hangman.py
+++ Hangman.py
@@ -1,9 +1,32 @@
+from random import randint as ri
+from os import system
+
+
+
 def findWord():
-    wordfile = open('words.txt', 'r')
+    wordfile = open('allmywords.txt', 'r')
     allwords = wordfile.readlines()
     wordfile.close()
+    return allwords[ri(0, len(allwords)-1)][0:-1]
 
-def createMan(numIncorrectGuesses):
+def createMan(incGuess):
+    if incGuess == 0:
+        return ''
+    if incGuess == 1:
+        return '|\n|\n|\n|\n|\n|'
+    if incGuess == 2:
+        return '________\n|/\n|\n|\n|\n|\n|'
+    if incGuess == 3:
+        return '________\n|/     |\n|\n|\n|\n|\n|'
+    if incGuess == 4:
+        return '________\n|/     |\n|       0\n|\n|\n|\n|'
+    if incGuess == 5:
+        return '________\n|/     |\n|       0\n|      /\\\n|\n|\n|'
+    if incGuess == 6:
+        return '________\n|/     |\n|       0\n|      /\\\n|        |\n|\n|'
+    if incGuess == 7:
+        return '________\n|/     |\n|       0\n|      /\\\n|        |\n|     /\\\n|'
+    
 
 def playGame():
     myWord = findWord()
@@ -17,6 +40,7 @@ if __name__ == '__main__':
         if useranswer == "no":
             print("Goodbye")
             break
+
 
 
 
