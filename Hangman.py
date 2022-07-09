18fd97e95ec359e5278dabc11dd7dd565da75896
Luke Hands <lukehands98@gmail.com>
2022-07-09 21:30:06 +0100

    Finished my mini terminal hang man game !

--- Hangman.py
+++ Hangman.py
@@ -30,6 +30,45 @@ def createMan(incGuess):
 
 def playGame():
     myWord = findWord()
+    gameProgress = ''
+    for i in range (len(myWord)):
+        gameProgress += '_'
+    incorrect = 0
+    guesses = []
+    while True:
+        _ = system('clear')
+        # print(word)
+        guessesString = ''
+        for i in range(len(guesses)):
+            if i != len(guesses) and i != 0:
+                guessesString += ', '
+            guessesString += guesses[i]
+        print(f'Past guesses: {guessesString}')
+        print(createMan(incorrect))
+        if gameProgress == myWord:
+            print(gameProgress)
+            print("You win")
+            break
+        if incorrect >= 7:
+            print('You Lose !')
+            print(f'The word was {myWord}.')
+            break
+        print(gameProgress)
+        print('Guess a letter!')
+        guess = input()
+        if guess not in guesses:
+            guesses.append(guess)
+        if guess in myWord:
+            # print(f'The letter {guess} is in the word.')
+            for i in range(len(myWord)):
+                if guess == myWord[i]:
+                    progressStart = gameProgress[0:i]
+                    progressEnd = gameProgress[i+1:]
+                    gameProgress = progressStart + guess + progressEnd
+        else:
+            incorrect += 1
+            
+        
 
 if __name__ == '__main__':
     while True:
