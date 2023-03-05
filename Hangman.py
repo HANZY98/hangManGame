65b3c6f9debef0e9f0e3da9f12b45793e1af2f9d
21012259-Luke Hands <91289931+HANZY98@users.noreply.github.com>
2023-03-05 21:52:03 +0000

    Add files via upload
    New most updates version of HangMan !

--- Hangman.py
+++ Hangman.py
@@ -1,88 +1,184 @@
-from random import randint as ri
-from os import system
+import random
 
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
 
+words = ["beans", "chocolate", "teeth"]
 
-def findWord():
-    wordfile = open('allmywords.txt', 'r')
-    allwords = wordfile.readlines()
-    wordfile.close()
-    return allwords[ri(0, len(allwords)-1)][0:-1]
+correctGuess = False
 
-def createMan(incGuess):
-    if incGuess == 0:
-        return ''
-    if incGuess == 1:
-        return '|\n|\n|\n|\n|\n|'
-    if incGuess == 2:
-        return '________\n|/\n|\n|\n|\n|\n|'
-    if incGuess == 3:
-        return '________\n|/     |\n|\n|\n|\n|\n|'
-    if incGuess == 4:
-        return '________\n|/     |\n|       0\n|\n|\n|\n|'
-    if incGuess == 5:
-        return '________\n|/     |\n|       0\n|      /\\\n|\n|\n|'
-    if incGuess == 6:
-        return '________\n|/     |\n|       0\n|      /\\\n|        |\n|\n|'
-    if incGuess == 7:
-        return '________\n|/     |\n|       0\n|      /\\\n|        |\n|     /\\\n|'
+ran_word = random.choice(words)
+
+word = []
+
+length_word = []
+
+inc_guesses = 0
+
+usererror = False
+
+def sortWords():
+    i = 0
+    while i < len(ran_word):
+        length_word.append('_')
+        newPos = ran_word[i]
+        word.append(newPos)
+        i += 1
+
+def reset():
+    inc_guesses = 0
+    ran_word = random.choice(words)
+    word.clear()
+    length_word.clear()
+
+sortWords()
+
+print('Welcome to Hangman !')
+
+print('\n')
+
+myinp = input('This will contain rules, press y to explain the rules or any other key to continue straight to the game\n')
+
+myinp.lower()
+
+if myinp == 'y':
+    print('print rules')
+else:
+    print('continuing to game...')
+    print('\n')
+
+while correctGuess == False:
+
+    if str(length_word).strip('[]') == str(word).strip('[]'):
+        print(*length_word)
+        print('You have won !')
+        break
     
+    print('\n')
+    print(*length_word)
+    print('\n')
+    letter_Guess = input('please take a guess !')
+    letter_Guess.lower()
 
-def playGame():
-    myWord = findWord()
-    gameProgress = ''
-    for i in range (len(myWord)):
-        gameProgress += '_'
-    incorrect = 0
-    guesses = []
-    while True:
-        _ = system('clear')
-        # print(word)
-        guessesString = ''
-        for i in range(len(guesses)):
-            if i != len(guesses) and i != 0:
-                guessesString += ', '
-            guessesString += guesses[i]
-        print(f'Past guesses: {guessesString}')
-        print(createMan(incorrect))
-        if gameProgress == myWord:
-            print(gameProgress)
-            print("You win")
-            break
-        if incorrect >= 7:
-            print('You Lose !')
-            print(f'The word was {myWord}.')
-            break
-        print(gameProgress)
-        print('Guess a letter!')
-        guess = input()
-        if guess not in guesses:
-            guesses.append(guess)
-        if guess in myWord:
-            # print(f'The letter {guess} is in the word.')
-            for i in range(len(myWord)):
-                if guess == myWord[i]:
-                    progressStart = gameProgress[0:i]
-                    progressEnd = gameProgress[i+1:]
-                    gameProgress = progressStart + guess + progressEnd
-        else:
-            incorrect += 1
-            
+    
+    if letter_Guess not in word:
+        print('This is incorrect\n')
+        print(HANGMANPICS[inc_guesses] + '\n')
+        inc_guesses += 1
+        if(inc_guesses > 7):
+            print('Oh no ! Frankie is nearly hung')
+        if(inc_guesses == 7):
+            print('Unfortunately Frankie has been hung, all is lost')
+            playagain = input('Would You like to play again ? (Y/N)')
+            playagain.lower()
+            if playagain == 'n':
+                correctGuess = True
+            elif playagain == 'y':
+                reset()
+                sortWords()
+            else:
+                playagain2 = input('Please enter (Y/N)')
+                if playagain2 == 'n':
+                    correctGuess = True
+                elif playagain2 == 'y':
+                    reset()
+                    sortWords()
+                else:
+                    break
         
+    if letter_Guess in length_word and j == len(word):
+        print('You have already used that letter')
+        
+     
+    j=0
+    while j < len(word):
+        
+        if letter_Guess == word[j] and length_word[j] == "_":
+            length_word.pop(j)
+            length_word.insert(j, letter_Guess)
+        
+        j += 1
 
-if __name__ == '__main__':
-    while True:
-        print("Would you like to play hangman?")
-        useranswer = input()
-        if useranswer == "yes":
-            playGame()
-        if useranswer == "no":
-            print("Goodbye")
-            break
+print('Thanks for playing ! See you again soon !')
 
+
+
+#THINGS TO DO!
+#add more words
+#if play again is not entered correctly, then make sure it asks them again
+#Make sure that re-setting the game makes it fully playable.
+#Re-factor, turn correct guess loop into a playgame function
 
 
 
 
 
     
+
+
+        
+    
+        
+    
+
+            
+
+
+
+        
+    
+
+
+
+
+    
+
+
